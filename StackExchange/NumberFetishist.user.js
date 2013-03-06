f98be9fbb91dde26b16b06976a1a487d57ddf72b
Robert Zenz <Bobby@bonsaimind.org>
2013-03-06 22:09:32 +0100

    Added.

---
+++ StackExchange/NumberFetishist.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        NumberFetishist
+// @namespace   StackExchange
+// @include     http://meta.stackoverflow.com/*
+// @version     1
+// ==/UserScript==
+
+
+/*
+ * Written by Robert 'Bobby' Zenz
+ * Public Domain or CC-Zero
+ */
+
+links = document.getElementsByTagName("a");
+
+for(var idx = 0; idx < links.length; idx++) {
+	var link = links[idx];
+	if(
+		link.className == "question-hyperlink" ||
+		link.className == "comment-user" ||
+		link.className == "profile-link" ||
+		link.parentNode.className == "user-details") {
+		var id = link.href.split("/")[4];
+		link.innerHTML = "#" + id + " " + link.innerHTML;
+	}
+}
